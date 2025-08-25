ab930dafb38b2fd30f16f2db545e7d282d1da022
syedabdullah56 <syedabdullah8750@gmail.com>
2025-08-25 23:35:48 +0500

    Day 2 Completed Today I studied about making flowcharts and writing pseudocodes for the problems

---
+++ DAY 2 LEC 1(FLOWCHART+PSEUDOCODE)/main.cpp
@@ -0,0 +1,18 @@
+//Q3:No is even or odd
+//Input n
+//if (n%2==0)
+   //print "even"
+   //else
+      //print "odd "
+// Exit
+
+// Q4:Find the sum of Numbers till N
+// Input N
+// count=1,sum=0
+//while (count<=n){
+    // sum=sum+count;
+    // count=count+1;
+//}
+// print sum
+// Exit
+
