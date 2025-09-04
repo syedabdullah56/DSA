25bc6fbda60d880a412048c7640575e3d96f88d2
syedabdullah56 <syedabdullah8750@gmail.com>
2025-09-04 13:17:48 +0500

    Day 3 ,Lecture 1 Completed

--- DAY 3 LEC 1(FLOWCHART+PSEUDOCODE)/main.cpp
+++ DAY 3 LEC 1(FLOWCHART+PSEUDOCODE)/main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
-using namespace std;
+//No is prime or not
+//Input n
+//i=2
+//while i<=(n-1){
+//  if(n%i==0){print "non prime"}
+//  else{ i+=1}
+//}
+//print prime
+//Exit
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+// Calculate simple interest
+// Input p,r,t
+// Initialize i
+// i=(p*r*t)/100
+// Print i
+
+// MAX OF 2 numbers
+// Input a,b
+// if(a>b){print a}
+// else{print b}
+
+// Calculate Factorial till n
+// Input n
+// i=1
+// fact=1
+// while (i<=n){
+//     fact=fact*i
+//     i++
+// }
+// Print fact
+
+// Check Age for Driving License
+// Input age
+// if(age>=18){print "Eligible"}
+// else{print "Not Eligible"}
